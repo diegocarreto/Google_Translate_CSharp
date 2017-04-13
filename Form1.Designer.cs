338054689e95a9ca95567cc8c4995c1a751a696b
Diego <diego.ao.carreto@gmail.com>
2017-04-13 10:29:57 -0500

    Traductor Español – Inglés con Google Translate y C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Traductor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.txtTranslator1 = new System.Windows.Forms.TextBox();
+            this.txtTranslator2 = new System.Windows.Forms.TextBox();
+            this.cmbLanguage = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(505, 228);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(75, 23);
+            this.btnTranslate.TabIndex = 0;
+            this.btnTranslate.Text = "Traducir";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // txtTranslator1
+            // 
+            this.txtTranslator1.Location = new System.Drawing.Point(12, 12);
+            this.txtTranslator1.Multiline = true;
+            this.txtTranslator1.Name = "txtTranslator1";
+            this.txtTranslator1.Size = new System.Drawing.Size(281, 210);
+            this.txtTranslator1.TabIndex = 1;
+            // 
+            // txtTranslator2
+            // 
+            this.txtTranslator2.Location = new System.Drawing.Point(299, 12);
+            this.txtTranslator2.Multiline = true;
+            this.txtTranslator2.Name = "txtTranslator2";
+            this.txtTranslator2.Size = new System.Drawing.Size(281, 210);
+            this.txtTranslator2.TabIndex = 2;
+            // 
+            // cmbLanguage
+            // 
+            this.cmbLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLanguage.FormattingEnabled = true;
+            this.cmbLanguage.Items.AddRange(new object[] {
+            "English/Spanish",
+            "Spanish/English"});
+            this.cmbLanguage.Location = new System.Drawing.Point(12, 230);
+            this.cmbLanguage.Name = "cmbLanguage";
+            this.cmbLanguage.Size = new System.Drawing.Size(281, 21);
+            this.cmbLanguage.TabIndex = 39;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 256);
+            this.Controls.Add(this.cmbLanguage);
+            this.Controls.Add(this.txtTranslator2);
+            this.Controls.Add(this.txtTranslator1);
+            this.Controls.Add(this.btnTranslate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Traductor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.TextBox txtTranslator1;
+        private System.Windows.Forms.TextBox txtTranslator2;
+        private System.Windows.Forms.ComboBox cmbLanguage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Traductor
+{
+    public partial class Form1 : Form
+    {
+        #region [Builder]
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region [Methods]
+
+        /// <summary>
+        /// Obtiene la traduccion del texto
+        /// </summary>
+        /// <param name="text">Texto a traducir</param>
+        /// <param name="from">Idioma origen</param>
+        /// <param name="to">Idioma destino</param>
+        /// <returns>Texto traducido</returns>
+        public static string Translate(string text, string from, string to)
+        {
+            var page = string.Empty;
+            var url = string.Empty;
+            var wc = new WebClient();
+
+            try
+            {
+
+                wc.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0");
+                wc.Headers.Add(HttpRequestHeader.AcceptCharset, "UTF-8");
+                wc.Encoding = Encoding.UTF8;
+
+                url = string.Format(@"http://translate.google.com.tr/m?hl=en&sl={0}&tl={1}&ie=UTF-8&prev=_m&q={2}", from, to, Uri.EscapeUriString(text));
+
+                page = wc.DownloadString(url);
+            }
+            catch (Exception ex)
+            {
+                return "Error";
+            }
+
+            page = page.Remove(0, page.IndexOf("<div dir=\"ltr\" class=\"t0\">")).Replace("<div dir=\"ltr\" class=\"t0\">", "");
+
+            var last = page.IndexOf("</div>");
+
+            page = page.Remove(last, page.Length - last);
+
+            return page;
+        }
+
+        /// <summary>
+        /// Realiza la traduccion del texto
+        /// </summary>
+        private void Translation()
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            if (!string.IsNullOrEmpty(this.txtTranslator1.Text))
+            {
+                try
+                {
+                    var translation = String.Empty;
+
+                    if (this.cmbLanguage.SelectedIndex.Equals(0))
+                        translation = Translate(this.txtTranslator1.Text, "en", "es");
+                    else
+                        translation = Translate(this.txtTranslator1.Text, "es", "en");
+
+                    this.txtTranslator2.Text = translation;
+
+                    this.txtTranslator2.SelectAll();
+                    this.txtTranslator2.Focus();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            Cursor.Current = Cursors.Default;
+        }
+
+        #endregion
+
+        #region [Events]
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cmbLanguage.SelectedIndex = 0;
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            this.Translation();
+        } 
+
+        #endregion
+    }
+}
